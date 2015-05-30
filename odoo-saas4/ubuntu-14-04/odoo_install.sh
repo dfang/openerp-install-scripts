949cea4556981ffd8984a8b00eec05e8cc4b9734
dfang <df1228@gmail.com>
2015-05-30 10:07:05 +0800

    MISC - Remove openerp7 and add depth option to git clone

--- odoo-saas4/ubuntu-14-04/odoo_install.sh
+++ odoo-saas4/ubuntu-14-04/odoo_install.sh
@@ -3,7 +3,7 @@
 # Script for Installation: ODOO Saas4/Trunk server on Ubuntu 14.04 LTS
 # Author: André Schenkels, ICTSTUDIO 2014
 #-------------------------------------------------------------------------------
-#  
+#
 # This script will install ODOO Server on
 # clean Ubuntu 14.04 Server
 #-------------------------------------------------------------------------------
@@ -12,10 +12,10 @@
 # odoo-install
 #
 # EXAMPLE:
-# ./odoo-install 
+# ./odoo-install
 #
 ################################################################################
- 
+
 ##fixed parameters
 #openerp
 OE_USER="odoo"
@@ -41,7 +41,7 @@ sudo apt-get upgrade -y
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
 sudo apt-get install postgresql -y
-	
+
 echo -e "\n---- PostgreSQL $PG_VERSION Settings  ----"
 sudo sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/postgresql/9.3/main/postgresql.conf
 
@@ -53,13 +53,13 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 #--------------------------------------------------
 echo -e "\n---- Install tool packages ----"
 sudo apt-get install wget subversion git bzr bzrtools python-pip -y
-	
+
 echo -e "\n---- Install python packages ----"
 sudo apt-get install python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf -y
-	
+
 echo -e "\n---- Install python libraries ----"
 sudo pip install gdata
-	
+
 echo -e "\n---- Create ODOO system user ----"
 sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
 
@@ -71,7 +71,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-sudo git clone --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+sudo git clone --branch $OE_VERSION --depth 1 https://www.github.com/odoo/odoo $OE_HOME_EXT/
 
 echo -e "\n---- Create custom module directory ----"
 sudo su $OE_USER -c "mkdir $OE_HOME/custom"
@@ -179,6 +179,6 @@ sudo chown root: /etc/init.d/$OE_CONFIG
 
 echo -e "* Start ODOO on Startup"
 sudo update-rc.d $OE_CONFIG defaults
- 
+
 echo "Done! The ODOO server can be started with /etc/init.d/$OE_CONFIG"
 
